004c4541bb639a986b1cd7eb050c7cdf5b5812cb
hahuy1708 <hahuy17082005@gmail.com>
2025-07-19 18:59:38 +0700

    add pb+nvpb

--- 1907_PHP/QLNS/xemthongtinnvpb.php
+++ 1907_PHP/QLNS/xemthongtinnvpb.php
@@ -1,5 +1,20 @@
 <?php
-require_once("conn.php");
+require_once("csdl.php");
 
+$idpb = $_GET['IDPB'];
+
+$sql = "Select * from nhanvien where IDPB = '$idpb'";
+$result = mysqli_query($link, $sql);
+echo "<table border='1' width='100%'>";
+echo "<caption>Danh sách nhân viên phòng ban</caption>";
+echo "<tr><th>Mã</th><th>Tên</th><th>Địa chỉ</th></tr>";
+
+while($row = mysqli_fetch_array($result,MYSQLI_BOTH)){
+    echo '<tr><td>' . $row['IDNV'] . '</td><td>' . $row['Hoten'] . '</td><td>' . $row['Diachi'] . '</td></tr>';
+}
+echo "</table>";
+
+mysqli_free_result($result);
+mysqli_close($link);
 
 ?>
--- 1907_PHP/QLNS/xemthongtinpb.php
+++ 1907_PHP/QLNS/xemthongtinpb.php
@@ -1,19 +1,23 @@
 <?php
 require_once("csdl.php");
 
-$sql = "SELECT * FROM nhanvien";
+$sql = "SELECT * FROM phongban";
 $result = mysqli_query($link, $sql);
 
 echo "<table border='1' width='100%'>";
 echo "<caption>Danh sách phòng ban</caption>";
-echo "<tr><th>Mã</th><th>Tên phòng ban</th><th>Mô tả</th></tr>";
+echo "<tr><th>Mã</th><th>Tên phòng ban</th><th>Mô tả</th><th>Xem chi tiết</th></tr>"; // thêm cột
 
-while($row = mysqli_fetch_array($result,MYSQLI_BOTH)){
-    echo '<tr><td>' . $row['IDPB'] . '</td><td>' . $row['TenPB'] . '</td><td>' . $row['Mota'] . '</td></tr>';
+while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
+    echo '<tr>
+            <td>' . $row['IDPB'] . '</td>
+            <td>' . $row['TenPB'] . '</td>
+            <td>' . $row['Mota'] . '</td>
+            <td><a href="xemthongtinnvpb.php?IDPB=' . $row['IDPB'] . '">Xem chi tiết</a></td>
+          </tr>';
 }
 echo "</table>";
 
 mysqli_free_result($result);
 mysqli_close($link);
-
 ?>
