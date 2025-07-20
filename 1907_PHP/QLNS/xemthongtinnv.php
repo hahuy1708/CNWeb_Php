b77bb7f5fc4562a8f51715f258d4310e99694164
hahuy1708 <hahuy17082005@gmail.com>
2025-07-20 14:16:39 +0700

    add find

--- 1907_PHP/QLNS/xemthongtinnv.php
+++ 1907_PHP/QLNS/xemthongtinnv.php
@@ -2,18 +2,56 @@
 require_once("csdl.php");
 
 $sql = "SELECT * FROM nhanvien";
-$result = mysqli_query($link, $sql);
 
-echo "<table border='1' width='100%'>";
-echo "<caption>Danh sách nhân viên</caption>";
-echo "<tr><th>Mã</th><th>Tên</th><th>Mã phòng ban</th><th>Địa chỉ</th></tr>";
+$keyword = isset($_GET['keyword']) ? trim($_GET['keyword']) : '';
 
-while($row = mysqli_fetch_array($result,MYSQLI_BOTH)){
-    echo '<tr><td>' . $row['IDNV'] . '</td><td>' . $row['Hoten'] . '</td><td>' . $row['IDPB'] . '</td><td>' . $row['Diachi'] . '</td></tr>';
+if ($keyword != '') {
+    $sql .= " WHERE Hoten LIKE '%$keyword%' OR IDNV LIKE '%$keyword%'";
 }
-echo "</table>";
 
+$result = mysqli_query($link, $sql);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tìm kiếm nhân viên</title>
+</head>
+<body>
+
+<h2>Tìm kiếm nhân viên</h2>
+
+<form method="GET" action="xemthongtinnv.php">
+    <input type="text" name="keyword" placeholder="Nhập tên hoặc mã nhân viên" value="<?php echo htmlspecialchars($keyword); ?>">
+    <input type="submit" value="Tìm">
+</form>
+
+<br>
+
+<table border="1" width="100%">
+    <caption>Danh sách nhân viên</caption>
+    <tr>
+        <th>Mã</th>
+        <th>Tên</th>
+        <th>Mã phòng ban</th>
+        <th>Địa chỉ</th>
+    </tr>
+
+    <?php while($row = mysqli_fetch_array($result, MYSQLI_BOTH)) { ?>
+        <tr>
+            <td><?php echo $row['IDNV']; ?></td>
+            <td><?php echo $row['Hoten']; ?></td>
+            <td><?php echo $row['IDPB']; ?></td>
+            <td><?php echo $row['Diachi']; ?></td>
+        </tr>
+    <?php } ?>
+
+</table>
+
+<?php
 mysqli_free_result($result);
 mysqli_close($link);
-
 ?>
+
+</body>
+</html>
--- 1907_PHP/QLNS/xemthongtinpb.php
+++ 1907_PHP/QLNS/xemthongtinpb.php
@@ -2,11 +2,18 @@
 require_once("csdl.php");
 
 $sql = "SELECT * FROM phongban";
+
+$keyword = isset($_GET['keyword']) ? trim($_GET['keyword']) : '';
+
+if ($keyword != '') {
+    $sql .= " WHERE TenPB LIKE '%$keyword%' OR IDPB LIKE '%$keyword%'";
+}
+
 $result = mysqli_query($link, $sql);
 
 echo "<table border='1' width='100%'>";
 echo "<caption>Danh sách phòng ban</caption>";
-echo "<tr><th>Mã</th><th>Tên phòng ban</th><th>Mô tả</th><th>Xem chi tiết</th></tr>"; // thêm cột
+echo "<tr><th>Mã</th><th>Tên phòng ban</th><th>Mô tả</th><th>Xem chi tiết</th></tr>"; 
 
 while($row = mysqli_fetch_array($result, MYSQLI_BOTH)){
     echo '<tr>
@@ -21,3 +28,20 @@ echo "</table>";
 mysqli_free_result($result);
 mysqli_close($link);
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tìm kiếm nhân viên</title>
+</head>
+<body>
+
+<h2>Tìm kiếm phòng ban</h2>
+
+<form method="GET" action="xemthongtinpb.php">
+    <input type="text" name="keyword" placeholder="Nhập tên hoặc mã phòng ban" value="<?php echo htmlspecialchars($keyword); ?>">
+    <input type="submit" value="Tìm">
+</form>
+
+</body>
+</html>
